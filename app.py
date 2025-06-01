d4e2245077136da283def8f2e0885c10cdaafde2
Ivan Kirika <ivtiks@vk.com>
2025-06-01 21:07:21 +0500

    Update app.py

--- app.py
+++ app.py
@@ -32,4 +32,4 @@ def index():
                            q3_number=q3_number, q3_text=q3_text, q3_discipline=q3_discipline)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5001)
