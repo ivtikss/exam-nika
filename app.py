6c68c5ab45d3df018e1121eaaa656a874ce2231b
Ivan Kirika <ivtiks@vk.com>
2025-06-01 21:19:39 +0500

    Update app.py

--- app.py
+++ app.py
@@ -6,21 +6,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # Выбор 1-го вопроса
+    # Вопрос 1 из ПАХТ
     q1_discipline = 'ПАХТ'
     q1 = random.choice(questions[q1_discipline])
     q1_number = q1.split('. ')[0]
     q1_text = q1[len(q1_number) + 2:]
 
-    # Выбор 2-го вопроса с указанием дисциплины
-    q2_disciplines = 'МАХП'
+    # Вопрос 2 из МАХП
+    q2_discipline = 'МАХП'
     q2 = random.choice(questions[q2_discipline])
     q2_number = q2.split('. ')[0]
     q2_text = q2[len(q2_number) + 2:]
 
-    # Выбор 3-го вопроса
-    excluded_disciplines = [q1_discipline] + [q2_discipline]
-    other_disciplines = [d for d in questions if d not in excluded_disciplines]
+    # Вопрос 3 из других дисциплин
+    excluded = ['ПАХТ', 'МАХП']
+    other_disciplines = [d for d in questions if d not in excluded]
     q3_discipline = random.choice(other_disciplines)
     q3 = random.choice(questions[q3_discipline])
     q3_number = q3.split('. ')[0]
@@ -32,4 +32,4 @@ def index():
                            q3_number=q3_number, q3_text=q3_text, q3_discipline=q3_discipline)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(debug=True, host='0.0.0.0', port=5000)
