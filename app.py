8e0edecf1b42622aa05bc100f964afd935a0397d
Ivan Kirika <ivtiks@vk.com>
2025-06-01 21:01:05 +0500

    Update app.py

--- app.py
+++ app.py
@@ -7,23 +7,19 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     # Выбор 1-го вопроса
-    q1_discipline = 'Конструирование элементов гибких производственных систем'
+    q1_discipline = 'ПАХТ'
     q1 = random.choice(questions[q1_discipline])
     q1_number = q1.split('. ')[0]
     q1_text = q1[len(q1_number) + 2:]
 
     # Выбор 2-го вопроса с указанием дисциплины
-    q2_disciplines = [
-        'Наладка станков с числовым программным управлением',
-        'Программирование станков с числовым программным управлением'
-    ]
-    q2_discipline = random.choice(q2_disciplines)
+    q2_disciplines = 'МАХП'
     q2 = random.choice(questions[q2_discipline])
     q2_number = q2.split('. ')[0]
     q2_text = q2[len(q2_number) + 2:]
 
     # Выбор 3-го вопроса
-    excluded_disciplines = [q1_discipline] + q2_disciplines
+    excluded_disciplines = [q1_discipline] + [q2_discipline]
     other_disciplines = [d for d in questions if d not in excluded_disciplines]
     q3_discipline = random.choice(other_disciplines)
     q3 = random.choice(questions[q3_discipline])
